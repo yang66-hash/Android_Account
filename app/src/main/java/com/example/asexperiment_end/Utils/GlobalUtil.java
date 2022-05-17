e0feca0bcfb191fb75204303799508f9cc1f5077
Riceer <chenxiaomofa@gmail.com>
2022-05-17 21:53:48 +0800

    添加日历选项，可以选择记的账目的日期，改了点颜色，改成中文

--- app/src/main/java/com/example/asexperiment_end/Utils/GlobalUtil.java
+++ app/src/main/java/com/example/asexperiment_end/Utils/GlobalUtil.java
@@ -54,8 +54,8 @@ public class GlobalUtil {
             R.drawable.icon_ticket_white,  R.drawable.icon_book_white,
             R.drawable.icon_medical_white,R.drawable.icon_transfer_white
     };
-    private static String[] costTitle = { "General","food", "Drinks","Groceries", "Shopping", "Personal","Entertain","Movies", "Social", "Transport",
-            "App Store","Mobile","Computer","Gifts", "Housing", "Travel","Tickets","Books", "Medical","Transfer"};
+    private static String[] costTitle = { "总体","食物", "饮料","杂货", "购物", "个人","娱乐","电影", "社交", "交通",
+            "应用","手机","电脑","礼物", "家居", "旅游","门票","书", "医疗","车票"};
 
 
     private static int[] earnIconRes = {
@@ -64,7 +64,7 @@ public class GlobalUtil {
             R.drawable.icon_parttime_white,R.drawable.icon_bonus_white,
             R.drawable.icon_investment_white};
 
-    private static String[] earnTitle = {"General", "Reimburse", "Salary","RedPocket","Part-time", "Bonus","Investment"};
+    private static String[] earnTitle = {"总体", "报销", "工资","红包","兼职", "奖金","投资"};
 
     //只是提供一个单实例
     public static GlobalUtil getInstance(){
